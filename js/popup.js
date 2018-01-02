90458b90e93a670ec2df9bbdb3cac3fb8be0dc4b
James Hamann <jameshamann@me.com>
2018-01-02 12:07:11 +0000

    adds bch and ripple

--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
       $.ajax({
-          url: "https://min-api.cryptocompare.com/data/pricemulti?fsyms=ETH,BTC,LTC&tsyms=USD,GBP",
+          url: "https://min-api.cryptocompare.com/data/pricemulti?fsyms=ETH,BTC,LTC,XRP,BCH&tsyms=USD,GBP",
           data: {
               txtsearch: $('#appendedInputButton').val()
           },
@@ -13,6 +13,9 @@ $(document).ready(function(){
             document.getElementById("bitcoin").innerHTML="£"+data.BTC.GBP;
             document.getElementById("ethereum").innerHTML="£"+data.ETH.GBP;
             document.getElementById("litecoin").innerHTML="£"+data.LTC.GBP;
+            document.getElementById("ripple").innerHTML="£"+data.XRP.GBP;
+            document.getElementById("btcash").innerHTML="£"+data.BCH.GBP;
+
           },
           error: function (xhr, status) {
               alert("Sorry, there was a problem loading the latest prices! Please try again");
