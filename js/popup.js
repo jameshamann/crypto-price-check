e3b149d1447f86b7a38ba54f4a1c74a9571a649e
James Hamann <jameshamann@me.com>
2018-01-02 12:20:05 +0000

    adds usd values to table

--- js/popup.js
+++ js/popup.js
@@ -10,12 +10,16 @@ $(document).ready(function(){
           success: function (data) {
             console.log(data.BTC.GBP)
             var coinPrice = data
-            document.getElementById("bitcoin").innerHTML="£"+data.BTC.GBP;
-            document.getElementById("ethereum").innerHTML="£"+data.ETH.GBP;
-            document.getElementById("litecoin").innerHTML="£"+data.LTC.GBP;
-            document.getElementById("ripple").innerHTML="£"+data.XRP.GBP;
-            document.getElementById("btcash").innerHTML="£"+data.BCH.GBP;
-
+            document.getElementById("bitcoingbp").innerHTML="£"+data.BTC.GBP;
+            document.getElementById("bitcoinusd").innerHTML="$"+data.BTC.USD;
+            document.getElementById("ethereumgbp").innerHTML="£"+data.ETH.GBP;
+            document.getElementById("ethereumusd").innerHTML="$"+data.ETH.USD;
+            document.getElementById("litecoingbp").innerHTML="£"+data.LTC.GBP;
+            document.getElementById("litecoinusd").innerHTML="$"+data.LTC.USD;
+            document.getElementById("ripplegbp").innerHTML="£"+data.XRP.GBP;
+            document.getElementById("rippleusd").innerHTML="$"+data.XRP.USD;
+            document.getElementById("btcashgbp").innerHTML="£"+data.BCH.GBP;
+            document.getElementById("btcashusd").innerHTML="$"+data.BCH.USD;
           },
           error: function (xhr, status) {
               alert("Sorry, there was a problem loading the latest prices! Please try again");
